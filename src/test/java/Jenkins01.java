62c770ba25bd62ae4f9ff42a6ffcb2962d598948
Huseyin Kartal <98432117+huseyinkartal1@users.noreply.github.com>
2022-11-03 18:37:55 +0300

    Update Jenkins01.java

--- src/test/java/Jenkins01.java
+++ src/test/java/Jenkins01.java
@@ -12,7 +12,7 @@ public class Jenkins01 extends BaseTest {
 
         Thread.sleep(3000);
 
-
+System.out.println("webhook check");
         System.out.println("commits observe webhook");
 
         Assert.assertEquals(driver.getTitle(),"apple");
